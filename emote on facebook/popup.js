def3473ff11f0030f26aec0b16fba6e94bfc2bc6
mrspyyt <piotrus.dudek@op.pl>
2021-09-30 13:19:00 +0200

    Create popup.js

---
+++ emote on facebook/popup.js
@@ -0,0 +1,33 @@
+function updateReplace() {
+    if (document.getElementById("replaceSwitch").checked) {
+      document.getElementById("replaceSwitch").checked = false;
+      chrome.storage.local.set({
+        emote_replace: "off"
+      }, function() {});
+    } else {
+      document.getElementById("replaceSwitch").checked = true;
+      chrome.storage.local.set({
+        emote_replace: "on"
+      }, function() {});
+    }
+  }
+  
+  chrome.storage.local.get({
+    emote_replace: 'on'
+  }, function(data) {
+  
+    if (data.emote_replace == 'off') {
+      document.getElementById("replaceSwitch").checked = false;
+    } else {
+      console.log("test");
+      document.getElementById("replaceSwitch").checked = true;
+    }
+  });
+  
+  
+  document.getElementById("replaceSwitch").parentElement.addEventListener('click', function() {
+    updateReplace();
+  });
+  document.getElementById("optionsLink").addEventListener("click", function() {
+    chrome.runtime.openOptionsPage();
+  });
